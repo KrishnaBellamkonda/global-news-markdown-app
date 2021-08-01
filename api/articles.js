0f1854673acd563151791281b7406dd12e8d3857
KrishnaBellamkonda <87589090+KrishnaBellamkonda@users.noreply.github.com>
2021-08-01 09:41:18 +0530

    Update articles.js

--- api/articles.js
+++ api/articles.js
@@ -10,10 +10,10 @@ async function getData(url=URL){
 
 }
 
-async function completeAPI(){
+async function completeAPI(bgColor, textColor, headingColor, borderColor){
     const data = await getData();
     const random = getRadnomizedArticles(data['results'])
-    const html = htmlWorkflow(random)
+    const html = htmlWorkflow(random, bgColor, textColor, headingColor, borderColor)
     return html
 }
 
@@ -46,7 +46,16 @@ function getRadnomizedArticles(results, n_indices=2){
 }
 
 module.exports = async (req, res)=>{
-    const articles = await completeAPI()
+    
+    // Get the colors from request 
+    const body = req.body 
+    
+    const bgColor = body.bgColor || "white"
+    const textColor = body.textColor || "black"
+    const headingColor = body.headingColor || "rgb(30, 5, 141)"
+    const borderColor = body.borderColor || "rgb(218, 218, 218)"
+    
+    const articles = await completeAPI(bgColor, textColor, headingColor, borderColor)
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(articles)
 
